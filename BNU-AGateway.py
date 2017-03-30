89ba3c49286f5418a3638035ddd2c26e070caed5
Rudolflee <ilikemeat@qq.com>
2017-03-30 10:50:35 +0800

    优化提速
    各种情况登陆控制在0.1秒之内

--- BNU-AGateway.py
+++ BNU-AGateway.py
@@ -3,8 +3,8 @@ import urllib2
 import urllib
 import re
 
-username = "学号"
-password = "密码"
+username = "引号内填写学号"
+password = "引号内填写密码"
 
 def check():
     try:
@@ -12,6 +12,21 @@ def check():
         return True
     except urllib2.URLError as err:
         return False
+
+def force_logout():
+    postData_logout = {
+        "action": "logout",
+        "username": username,
+        "password": password,
+        "ajax": "1"
+    }
+    postData_logout = urllib.urlencode(postData_logout)
+    posturl_logout = "http://172.16.202.201:802/include/auth_action.php"
+    header_logout = {
+        "User-Agen": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36",
+        "Referer": "http://gw.bnu.edu.cn:803/srun_portal_phone.php?ac_id=1&"
+    }
+    request_logout = urllib2.Request(posturl_logout, postData_logout, header_logout)
 
 def login():
     posturl = "http://gw.bnu.edu.cn:803/srun_portal_phone.php?ac_id=1&"
@@ -32,36 +47,22 @@ def login():
     }
     postData = urllib.urlencode(postData)
     request = urllib2.Request(posturl, postData, header)
-    response = urllib2.urlopen(request)
-    return response.read()
-
-def force_logout():
-    postData_logout = {
-        "action": "logout",
-        "username": username,
-        "password": password,
-        "ajax": "1"
-    }
-    postData_logout = urllib.urlencode(postData_logout)
-    posturl_logout = "http://172.16.202.201:802/include/auth_action.php"
-    header_logout = {
-        "User-Agen": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36",
-        "Referer": "http://gw.bnu.edu.cn:803/srun_portal_phone.php?ac_id=1&"
-    }
-    request_logout = urllib2.Request(posturl_logout, postData_logout, header_logout)
-    response_logout = urllib2.urlopen(request_logout)
+    content = urllib2.urlopen(request).read()
+    if content.find("成功") != -1:
+        print '登录成功'
+        return
+    else:
+        force_logout()
+        request = urllib2.Request(posturl, postData, header)
+        content = urllib2.urlopen(request).read()
+        if content.find("用尽") != -1:
+            print '没救了，充钱吧'
+            return
+        else:
+            print '全部下线，登录成功'
+            return
 
 if check():
-    content = login()
-    if len(re.findall('成功', content)) == 0:
-        force_logout()
-        content = login()
-        m = re.findall('成功', content)
-        if len(m) == 0:
-            print '没得救了，充钱吧'
-        else:
-            print '登陆成功，已全部下线'
-    else:
-        print "登陆成功"
+    login()
 else:
     print '网络连接失败，请检查网络连接'
